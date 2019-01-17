41bf1f8f6f64be01cd264f6e42f56d17741e52b1
Saúl Ibarra Corretgé <s@saghul.net>
2019-01-17 07:16:29 +0100

    Python 3.7 support, tkake 2

--- setup.py
+++ setup.py
@@ -46,5 +46,6 @@ setup(name             = "aiodns",
           "Programming Language :: Python :: 3.4",
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7"
       ]
 )
