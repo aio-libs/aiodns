34cadc44c188535caf8425c804fbe32d35f1a537
Saúl Ibarra Corretgé <s@saghul.net>
2019-01-17 23:53:36 +0100

    Add test for processing queries with non-ASCII in their responses
    This is fixed in pycares.

--- tests.py
+++ tests.py
@@ -139,6 +139,11 @@ class DNSTest(unittest.TestCase):
         with self.assertRaises(aiodns.error.DNSError):
             self.loop.run_until_complete(f)
 
+    def test_query_bad_chars(self):
+        f = self.resolver.query('xn--cardeosapeluqueros-r0b.com', 'MX')
+        result = self.loop.run_until_complete(f)
+        self.assertTrue(result)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
