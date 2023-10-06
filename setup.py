7b86a3b4c555f34cf409f212fde620447dd04a98
Saúl Ibarra Corretgé <s@saghul.net>
2023-10-06 10:12:57 +0200

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -31,9 +31,10 @@ setup(name             = "aiodns",
           "Operating System :: Microsoft :: Windows",
           "Programming Language :: Python",
           "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.6",
-          "Programming Language :: Python :: 3.7",
           "Programming Language :: Python :: 3.8",
-          "Programming Language :: Python :: 3.9"
+          "Programming Language :: Python :: 3.9",
+          "Programming Language :: Python :: 3.10",
+          "Programming Language :: Python :: 3.11",
+          "Programming Language :: Python :: 3.12"
       ]
 )
