ddff5e0182548e8e895f02243d949d9ce700bf27
J. Nick Koston <nick@koston.org>
2025-05-01 16:19:44 -0500

    Use c-ares event thread when available
    This also means we do not care about which type of
    event loop is running

--- aiodns/__init__.py
+++ aiodns/__init__.py
@@ -102,7 +102,7 @@ class DNSResolver:
     def _get_future_callback(self) -> Tuple[asyncio.Future, Callable[[Any, int], asyncio.Handle | None]]:
         """Return a future and a callback to set the result of the future."""
         cb: Callable[[Any, int], asyncio.Handle | None]
-        future: asyncio.Future[Any] = self.loop.create_future()
+        future: "asyncio.Future[Any]" = self.loop.create_future()
         if self._event_thread:
             cb = functools.partial(self.loop.call_soon_threadsafe, self._callback, future)
         else:
