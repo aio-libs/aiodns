807f237d386737924e625cafe76814970457ff70
Joona Gynther <joona.gynther@f-secure.com>
2025-06-06 17:02:21 +0300

    fix return type for CNAME and SOA records

--- aiodns/__init__.py
+++ aiodns/__init__.py
@@ -172,7 +172,7 @@ class DNSResolver:
     @overload
     def query(
         self, host: str, qtype: Literal['CNAME'], qclass: Optional[str] = ...
-    ) -> asyncio.Future[list[pycares.ares_query_cname_result]]: ...
+    ) -> asyncio.Future[pycares.ares_query_cname_result]: ...
     @overload
     def query(
         self, host: str, qtype: Literal['MX'], qclass: Optional[str] = ...
@@ -192,7 +192,7 @@ class DNSResolver:
     @overload
     def query(
         self, host: str, qtype: Literal['SOA'], qclass: Optional[str] = ...
-    ) -> asyncio.Future[list[pycares.ares_query_soa_result]]: ...
+    ) -> asyncio.Future[pycares.ares_query_soa_result]: ...
     @overload
     def query(
         self, host: str, qtype: Literal['SRV'], qclass: Optional[str] = ...
@@ -204,7 +204,7 @@ class DNSResolver:
 
     def query(
         self, host: str, qtype: str, qclass: Optional[str] = None
-    ) -> asyncio.Future[list[Any]]:
+    ) -> asyncio.Future[list[Any] | Any]:
         try:
             qtype = query_type_map[qtype]
         except KeyError as e:
@@ -215,7 +215,7 @@ class DNSResolver:
             except KeyError as e:
                 raise ValueError(f'invalid query class: {qclass}') from e
 
-        fut: asyncio.Future[list[Any]]
+        fut: asyncio.Future[list[Any] | Any]
         fut, cb = self._get_future_callback()
         self._channel.query(host, qtype, cb, query_class=qclass)
         return fut
