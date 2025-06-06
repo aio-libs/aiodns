a324600b5ae09585ae5562073ba10ca9b14ba074
Sam Bull <aa6bs0@sambull.org>
2025-06-06 16:29:52 +0100

    Apply suggestions from code review

--- aiodns/__init__.py
+++ aiodns/__init__.py
@@ -204,7 +204,7 @@ class DNSResolver:
 
     def query(
         self, host: str, qtype: str, qclass: Optional[str] = None
-    ) -> asyncio.Future[list[Any] | Any]:
+    ) -> asyncio.Future[list[Any]] | asyncio.Future[Any]:
         try:
             qtype = query_type_map[qtype]
         except KeyError as e:
@@ -215,7 +215,7 @@ class DNSResolver:
             except KeyError as e:
                 raise ValueError(f'invalid query class: {qclass}') from e
 
-        fut: asyncio.Future[list[Any] | Any]
+        fut: asyncio.Future[list[Any]] | asyncio.Future[Any]
         fut, cb = self._get_future_callback()
         self._channel.query(host, qtype, cb, query_class=qclass)
         return fut
